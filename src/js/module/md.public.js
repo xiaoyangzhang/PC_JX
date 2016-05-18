2b08ec0c2862e2aa592ae7410589a633237fff43
xiongzl <xiongzl@jiuxiulvxing.com>
2016-05-18 17:07:05 +0800

    update <br>

--- src/js/module/md.public.js
+++ src/js/module/md.public.js
@@ -35,6 +35,30 @@ define(function (require, exports, module) {
 			});
 				_self.depath();
 		},
+		html_encode : function(str){   
+		  var s = "";   
+		  if (str.length == 0) return "";   
+		  s = str.replace(/&/g, "&gt;");   
+		  s = s.replace(/</g, "&lt;");   
+		  s = s.replace(/>/g, "&gt;");   
+		  s = s.replace(/ /g, "&nbsp;");   
+		  s = s.replace(/\'/g, "&#39;");   
+		  s = s.replace(/\"/g, "&quot;");   
+		  s = s.replace(/\n/g, "<br>");   
+		  return s;   
+		},
+		html_decode : function(str){
+		  var s = "";   
+		  if (str.length == 0) return "";   
+		  s = str.replace(/&gt;/g, "&");   
+		  s = s.replace(/&lt;/g, "<");   
+		  s = s.replace(/&gt;/g, ">");   
+		  s = s.replace(/&nbsp;/g, " ");   
+		  s = s.replace(/&#39;/g, "\'");   
+		  s = s.replace(/&quot;/g, "\"");   
+		  s = s.replace(/<br>/g, "\n");   
+		  return s;  
+		},
 		isLogin :function(data){
 			/* console.log(data); */
 			if(!data instanceof Object)
