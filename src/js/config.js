526744eb33204938183f952196b6b35096a8e171
houxiaohui <houxh@jiuxiulvxing.com>
2016-05-24 11:05:04 +0800

    update businessenter

--- src/js/config.js
+++ src/js/config.js
@@ -39,8 +39,8 @@ seajs.config({
 		"password":"module/md.password.js",
 		"merchant":"module/md.merchant.js",	
 		"passwordSucc":"module/md.passwordsucc.js"	,
-		"review":"module/md.review.js"	
-			
+		"review":"module/md.review.js",	
+		"businessenter":"module/md.businessenter.js"	
 			
     },
     preload: ["jquery","underscore"],/*配置提取预加载模块，全局模块，所以页面都需要使用的模块*/
