01f16b34579f1fba9f38fd56d7d6bf231e52b1fd
xinan <xinan@jiuxiulvxing.com>
2016-05-31 18:12:56 +0800

    评论

--- src/js/module/md.review.js
+++ src/js/module/md.review.js
@@ -73,7 +73,7 @@ define(function (require, exports, module) {
 		},
 		distanceFun :function(){
 			/* 根据页面高度判断 */
-			if($(".eredar-right").height() > $(".eredar-left").height()){
+			if($(".eredar-right").height() < $(".eredar-left").height()){
 				$public.depath();
 			}
 			else{
