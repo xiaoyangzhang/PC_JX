393e08a1e5d9e1209946f5f45ed7bc3f50961c24
xinan <xinan@jiuxiulvxing.com>
2016-06-30 17:56:25 +0800

    coupon

---
+++ src/js/module/md.addcoupon.js
@@ -0,0 +1,34 @@
+﻿define(function (require, exports, module) {
+	$addcoupon = function () {
+		this.init.apply(this, arguments);
+	};
+	$addcoupon.prototype = {
+		init:function(){
+		},
+		timeFun : function($time1,$time2){
+			var startime=$time1.val(),endtime=$time2.val();
+			var datanew1 = startime + " 00:00:00";
+			var datanew2 = endtime + " 00:00:00";
+			var data1 = new Date(datanew1);
+			var data2 = new Date(datanew2);
+			var val = parseInt(data2.getTime() - data1.getTime())/1000/60/60/24;
+			var interval = Math.abs(parseInt(data2.getTime() - data1.getTime())/1000/60/60/24);
+			if(startime != "" && endtime != "")
+			{
+				if(val < 0){
+					alert("开始时间必须早于结束时间")
+					$time1.val("");
+					$time2.val("");
+				}
+				if(interval>60){
+					alert("两者之间应该在60天之内");
+					$time1.val("");
+					$time2.val("");
+				}
+			}
+			else{
+			}
+		}
+	}
+	module.exports = new $addcoupon();
+});
