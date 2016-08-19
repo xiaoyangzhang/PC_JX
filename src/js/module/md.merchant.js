b19342bef91e3144790ad3e0b3e7b8c7167fa071
zhangxiaoyang <xiaoyangzhang1990@163.com>
2016-08-19 17:40:07 +0800

    1

--- src/js/module/md.merchant.js
+++ src/js/module/md.merchant.js
@@ -64,7 +64,7 @@ define(function (require, exports, module) {
 					ele:".niname",
 					datatype:"nmname",
 					nullmsg:"请填写信息",
-					errormsg:"昵称至少2个字符,最多15个字符！"
+					errormsg:"请输入2~15位字符，支持中文、字母、数字、下划线"
 					
 				}],validfm=$(".registerform").Validform(validoptions).addRule(rule);
 
