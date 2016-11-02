974fff83eff95cff90e7996133216fe0e171a4ea
xiongzl <xiongzl@jiuxiulvxing.com>
2016-11-02 10:42:06 +0800

    整理公共文件

--- src/js/other/public.js
+++ src/js/other/public.js
@@ -1,10 +1,3 @@
-/* function depath(){
-	var hh=$(".header").height(),fh=$(".footer").height(),el=$(".eredar-left"),er=$(".eredar-right"),
-		wdh=$(window).height()-hh-fh-102,auto_height=$(document).height()-hh-fh-60,
-		elh=el.height()+120,erh=er.height();
-	if(wdh>elh&&wdh>erh)
-		el.height(elh);
-	else
-		el.height(auto_height);
-}
-$(function(){depath();}); */
+/*! PC_JX - v1.0.0 - 2016-11-02 */
+
+/*! PC_JX xiongzhaoling 最后修改于： 2016-11-02 */
