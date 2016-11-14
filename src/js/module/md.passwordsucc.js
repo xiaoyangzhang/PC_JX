6a97c88e45b6035664c3e2618835e9ac92d52bf0
jinjl <jinjl@jiuxiulvxing.com>
2016-11-14 16:53:43 +0800

    修改续签图片  打包

--- src/js/module/md.passwordsucc.js
+++ src/js/module/md.passwordsucc.js
@@ -1,9 +1,3 @@
-<<<<<<< HEAD
-/*! PC_JX - v1.0.0 - 2016-11-09 */
+/*! PC_JX - v1.0.0 - 2016-11-14 */
 define(function(require,exports,module){$editer=require("editer"),$test=function(){this.init.apply(this,arguments)},$test.prototype={init:function(){$editer.distanceFun()}},module.exports=new $test});
-/*! PC_JX xiongzhaoling 最后修改于： 2016-11-09 */
-=======
-/*! PC_JX - v1.0.0 - 2016-11-10 */
-define(function(require,exports,module){$editer=require("editer"),$test=function(){this.init.apply(this,arguments)},$test.prototype={init:function(){$editer.distanceFun()}},module.exports=new $test});
-/*! PC_JX xiongzhaoling 最后修改于： 2016-11-10 */
->>>>>>> v_0_1_0_20161115
+/*! PC_JX xiongzhaoling 最后修改于： 2016-11-14 */
