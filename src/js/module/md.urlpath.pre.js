fc76e8f7887ba06136fa51b1d2e589e0394abe65
jinjl <jinjl@jiuxiulvxing.com>
2016-11-21 15:24:39 +0800

    商品上架后可以编辑图文详情

--- src/js/module/md.urlpath.pre.js
+++ src/js/module/md.urlpath.pre.js
@@ -1,3 +1,3 @@
-/*! PC_JX - v1.0.0 - 2016-11-07 */
+/*! PC_JX - v1.0.0 - 2016-11-21 */
 define(function(require,exports,module){module.exports={fileuploadURL:"http://filegw.jiuxiulvxing.com/filegw/file/upload_string",c_domain:"jiuxiulvxing.com",site_path:$("#root_path").val(),static_source:"http://s0.jiuxiulvxing.com/pre/PC_JX/",img_domain:"http://img.yimayholiday.com/v1/tfs/"}});
-/*! PC_JX xiongzhaoling 最后修改于： 2016-11-07 */
+/*! PC_JX xiongzhaoling 最后修改于： 2016-11-21 */
