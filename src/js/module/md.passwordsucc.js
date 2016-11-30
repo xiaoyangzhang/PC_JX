e10489c8492a8842744c3634c9d6c87c051dda08
ruihongran <ruihr@jiuxiulvxing.com>
2016-11-30 11:22:29 +0800

    压缩静态资源

--- src/js/module/md.passwordsucc.js
+++ src/js/module/md.passwordsucc.js
@@ -1,9 +1,3 @@
-<<<<<<< HEAD
 /*! PC_JX - v1.0.0 - 2016-11-29 */
 define(function(require,exports,module){$editer=require("editer"),$test=function(){this.init.apply(this,arguments)},$test.prototype={init:function(){$editer.distanceFun()}},module.exports=new $test});
 /*! PC_JX xiongzhaoling 最后修改于： 2016-11-29 */
-=======
-/*! PC_JX - v1.0.0 - 2016-11-24 */
-define(function(require,exports,module){$editer=require("editer"),$test=function(){this.init.apply(this,arguments)},$test.prototype={init:function(){$editer.distanceFun()}},module.exports=new $test});
-/*! PC_JX xiongzhaoling 最后修改于： 2016-11-24 */
->>>>>>> master
