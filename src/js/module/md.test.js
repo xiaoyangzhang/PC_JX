a6e932e75bc3e8370b48fcf25bc38a27d0e69986
ruihongran <ruihr@jiuxiulvxing.com>
2016-12-16 11:01:07 +0800

    压缩静态资源

--- src/js/module/md.test.js
+++ src/js/module/md.test.js
@@ -1,3 +1,3 @@
-/*! PC_JX - v1.0.0 - 2016-12-06 */
+/*! PC_JX - v1.0.0 - 2016-12-16 */
 define(function(require,exports,module){require("core"),require("widget"),require("datepicker"),require("uploadfiles"),require("validform"),require("dropdownlist"),$public=require("public"),$test=function(){this.init.apply(this,arguments)},$test.prototype={init:function(){$("#tm").datepicker(),$("#edu").selectlist({zIndex:10,width:200,height:32,onChange:function(){}});$(".registerform").Validform({tiptype:3,label:".label",showAllError:!0,ajaxPost:!0})}},module.exports=new $test});
-/*! PC_JX xiongzhaoling 最后修改于： 2016-12-06 */
+/*! PC_JX xiongzhaoling 最后修改于： 2016-12-16 */
